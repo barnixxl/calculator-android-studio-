cedc036b6264cf0e1a7bee937b147153d2e64923
misha <birinmisa15@gmail.com>
2026-02-15 16:27:24 +0300

    ready calculator

--- lib/main.dart
+++ lib/main.dart
@@ -10,58 +10,204 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'калькулятор',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Calculate(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class Calculate extends StatefulWidget{
+  const Calculate({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Calculate> createState() => _CalculateState();
+
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CalculateState extends State<Calculate>{
+  String display = '0';
+  double firstNum = 0;
+  double secondNum = 0;
+  String operation = '';
+  bool newNum = true;
 
-  void _incrementCounter() {
+  void numPress(String digit){
     setState(() {
-      _counter++;
+      if(newNum || display == '0'){
+        display = digit;
+        newNum = false;
+      }
+      else{
+        display = display + digit;
+      }
     });
+  }
+
+  void operationPress(String oper){
+    setState(() {
+      firstNum = double.parse(display);
+      operation = oper;
+      newNum = true;
+    });
+  }
+
+  void writeResult(){
+    setState(() {
+      secondNum = double.parse(display);
+
+      switch (operation){
+
+        case '+':
+          display = (firstNum + secondNum).toString();
+
+        case '-':
+          display = (firstNum - secondNum).toString();
+
+        case '/':
+          if(secondNum == 0){
+            display = "ошибка: деление на 0";
+            break;
+          }
+          else {
+            display = (firstNum / secondNum).toString();
+          }
+
+        case '%':
+          display = ((secondNum / 100) * firstNum).toString();
+
+        case '*':
+          display = (firstNum * secondNum).toString();
+
+        default:
+          return;
+      }
+
+      newNum = true;
+    });
+  }
+
+  void clear(){
+    setState(() {
+       display = '0';
+       firstNum = 0;
+       secondNum = 0;
+       operation = '';
+       newNum = true;
+    });
+  }
+
+  Color _buttonColor(String text){
+    switch (text){
+      case 'C':
+        return Colors.red;
+      case '=':
+        return Colors.green;
+      case '/':
+      case '+':
+      case '-':
+      case '%':
+      case '*':
+        return Colors.black;
+      default:
+        return Colors.grey;
+    }
+  }
+
+  void _display(String text){
+    if(text == 'C'){
+      clear();
+    }
+    else if(text == '='){
+      writeResult();
+    }
+    else if(text == '/' || text == '+' || text == '-' || text == '%' || text == '*' ){
+      operationPress(text);
+    }
+    else {
+      numPress(text);
+    }
+  }
+
+  Widget _buildButton(String text) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(6),
+        child: ElevatedButton(
+          onPressed: () => _display(text),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: _buttonColor(text),
+            foregroundColor: Colors.white,
+            padding: const EdgeInsets.all(20),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: Text(
+            text,
+            style: const TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+
+ Widget _buttonRow(List<String> buttons){
+    return Expanded(
+      child: Row(
+        children: buttons.map(
+                (text) => _buildButton(text)
+        ).toList(),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('калькулятор'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.grey[100],
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Text(
+                    display,
+                    style: const TextStyle(
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+
+          Expanded(
+            flex: 5,
+            child: Column(
+              children: [
+                _buttonRow(['C', '/', '%']),
+                _buttonRow(['7', '8', '9', '×']),
+                _buttonRow(['4', '5', '6', '-']),
+                _buttonRow(['1', '2', '3', '+']),
+                _buttonRow(['0', '00', '.', '=']),
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
